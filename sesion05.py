8c997b592fcd026ad920dcd00404f4cbbb9d4688
Wolf Course <wolfprogrammingcourse@gmail.com>
2024-08-08 16:25:44 -0400

    sesion05

---
+++ sesion05.py
@@ -0,0 +1,18 @@
+# Valor 10 en base decimal
+print ("Base decimal")
+print (10)
+# Valor 10 en binario
+print ("Base binaria")
+print (0b1010)
+# Valor 10 en octal
+print ("Base octal")
+print (0o12)
+# Valor 10 en hexadecimal
+print ("Base hexadecimal")
+print(0xa)
+
+
+#precision de 17 digitos, luego lo redondea
+var=0.9999999999999999
+print(type(var))
+print(var)
